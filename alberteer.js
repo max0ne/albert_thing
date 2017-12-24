e459e218af2f319acf479455a4a412cf1b99ff41
Mingfei Huang <himax1023@gmail.com>
2017-12-24 14:07:30 -0500

    flatten results

--- alberteer.js
+++ alberteer.js
@@ -1,25 +1,25 @@
+const debug = require('debug')('alberteer');
 const { browser, page, openBrowser, goto, click, button, closeBrowser, $ } = require('taiko');
 
 /**
  * browser code
  */
 function grabClasses() {
-  const classes = {};
+  const classes = [];
   const searchResults = $('#search-results').children().toArray();
-  let currentClass;
+  let classNumber, classTitle;
   searchResults.forEach((ele) => {
     if ($(ele).hasClass('class-title-header')) {
-      currentClass = $(ele).text();
-      classes[currentClass] = [];
+      [classNumber, classTitle] = $(ele).text().split(' - ');
       return;
     }
 
-    let section = $(ele).find('.section-content')[0];
+    const section = $(ele).find('.section-content')[0];
     if (section) {
       const sectionObj = $(section).find('.section-body').toArray().reduce((acc, curr) => {
         const [key, val] = $(curr).text().split(':');
         if (key && val) {
-          acc[key.trim()] = val.trim();
+          acc[key.trim().toLowerCase()] = val.trim();
         }
         return acc;
       }, {});
@@ -29,7 +29,9 @@ function grabClasses() {
         .filter((tt) => tt.length > 0)
         .join('\n');
       sectionObj.href = $(ele).attr('href');
-      classes[currentClass].push(sectionObj);
+      sectionObj.classNumber = classNumber;
+      sectionObj.classTitle = classTitle;
+      classes.push(sectionObj);
     }
   });
   return classes;
@@ -39,22 +41,31 @@ function grabClasses() {
  * @param {string} term `Spring 2018`
  * @param {string} school `Tandon - Grad`
  * @param {string} subject `Computer Science`
- * @return {promise<any>}
+ * @return {Promise<any[]>}
  */
 async function search(term, school, subject) {
   try {
     await openBrowser();
+    debug('open browser');
     await goto('https://m.albert.nyu.edu/app/catalog/classSearch');
+    debug('goto class search');
     await click($('.btn[data-id="term"]'));
+    debug('expand term');
     await click(term);
+    debug(`select term ${term}`);
     await click('Select School');
+    debug('expand school');
     await click(school);
+    debug(`select school ${school}`);
     await click('Subject');
+    debug('expand subject');
     await click(subject);
+    debug(`select subject ${subject}`);
     await click(button('Search'));
-
+    debug(`search`);
     return await page().evaluate(grabClasses);
   } catch (e) {
+    debug(e);
     throw e;
   } finally {
     if (browser()) {
@@ -64,7 +75,3 @@ async function search(term, school, subject) {
 }
 
 module.exports = { search };
-
-if (require && require.main == module) {
-  search('Spring 2018', 'Tandon - Grad', 'Computer Science').then(console.log).catch(console.error);
-}
